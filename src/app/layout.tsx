5124be671ea7343fbbd4e055a3dffe0c330ba6f2
Aditya-2aga <aditya.2aga@gmail.com>
2025-05-20 17:46:22 +0530

    Implemented pwa and manifest.json is created and edited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,18 @@ import '../styles/index.css';
 
 export const viewport = {
   width: 'device-width',
+  themeColor: '#2b63d9',
   initialScale: 1,
+  minimumScale: 1,
 };
 
 export const metadata = {
   title: 'LookScout',
   description: 'A frontend project',
+  manifest: '/manifest.json',
+  other: {
+    'LookScout': 'LookScout'
+  },
   icons: {
     icon: [
       { url: '/favicon.ico', type: 'image/x-icon' }
@@ -27,7 +33,7 @@ export default function RootLayout({
       <body>
       {children}
         <script id="dhws-errorTracker" src="/dhws-error-tracker.js"></script>
-  <script id="dhws-elementInspector" src="/dhws-web-inspector.js"></script>
+       <script id="dhws-elementInspector" src="/dhws-web-inspector.js"></script>
 </body>
     </html>
   );
